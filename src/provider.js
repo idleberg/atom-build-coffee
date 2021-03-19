f4ca0653086c58d2d4dbfa7cd4071e0f56e6f4b6
idleberg <git@idleberg.com>
2021-03-19 23:31:35 +0100

    build

---
+++ src/provider.js
@@ -0,0 +1,101 @@
+import { configSchema, getConfig } from './config';
+import { EventEmitter } from 'events';
+import { satisfyDependencies } from 'atom-satisfy-dependencies';
+import { spawnSync } from 'child_process';
+import { which } from './util';
+
+export { configSchema as config };
+
+export function provideBuilder() {
+  return class CoffeeProvider extends EventEmitter {
+    constructor(cwd) {
+      super();
+
+      this.cwd = cwd;
+      atom.config.observe('build-coffee.customArguments', () => this.emit('refresh'));
+    }
+
+    getNiceName() {
+      return 'CoffeeScript';
+    }
+
+    isEligible() {
+      if (getConfig('alwaysEligible') === true) {
+        return true;
+      }
+
+      const cmd = spawnSync(which(), ['coffee']);
+
+      return !cmd?.stdout?.toString()
+        ? false
+        : true;
+    }
+
+    settings() {
+      const errorMatch = [
+        '(?<file>([^:]+)):(?<line>\\d+):(?<col>\\d+): error: (?<message>.+)'
+      ];
+
+      const warningMatch = [
+        '(?<file>([^:]+)):(?<line>\\d+):(?<col>\\d+): warning: (?<message>.+)'
+      ];
+
+      // User settings
+      const customArguments = getConfig('customArguments').trim().split(' ');
+      customArguments.push('{FILE_ACTIVE}');
+
+      return [
+        {
+          name: 'CoffeeScript',
+          exec: 'coffee',
+          args: [ '--compile', '{FILE_ACTIVE}' ],
+          cwd: '{FILE_ACTIVE_PATH}',
+          sh: false,
+          keymap: 'cmd-alt-b',
+          atomCommandName: 'coffeescript:compile',
+          errorMatch: errorMatch,
+          warningMatch: warningMatch
+        },
+        {
+          name: 'CoffeeScript --bare',
+          exec: 'coffee',
+          args: [ '--compile', '--bare', '{FILE_ACTIVE}' ],
+          cwd: '{FILE_ACTIVE_PATH}',
+          sh: false,
+          keymap: 'cmd-alt-shift-b',
+          atomCommandName: 'coffeescript:compile-bare',
+          errorMatch: errorMatch,
+          warningMatch: warningMatch
+        },
+        {
+          name: 'CoffeeScript --map',
+          exec: 'coffee',
+          args: [ '--compile', '--map', '{FILE_ACTIVE}' ],
+          cwd: '{FILE_ACTIVE_PATH}',
+          sh: false,
+          keymap: 'cmd-alt-cmd-b',
+          atomCommandName: 'coffeescript:compile-and-create-map',
+          errorMatch: errorMatch,
+          warningMatch: warningMatch
+        },
+        {
+          name: 'CoffeeScript (user)',
+          exec: 'coffee',
+          args: customArguments,
+          cwd: '{FILE_ACTIVE_PATH}',
+          sh: false,
+          atomCommandName: 'coffeescript:compile-with-user-settings',
+          errorMatch: errorMatch,
+          warningMatch: warningMatch
+        }
+      ];
+    }
+  };
+}
+
+// This package depends on build, make sure it's installed
+export function activate() {
+  if (getConfig('manageDependencies') === true) {
+    satisfyDependencies('build-coffee');
+  }
+}
